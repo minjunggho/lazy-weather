b436c97a92984406c0551a10fa0f4f1ef8b85717
minjunggho <editstudio10@gmail.com>
2026-05-15 23:22:33 -0700

    Add fake weather advice endpoint

--- backend/app/main.py
+++ backend/app/main.py
@@ -5,3 +5,14 @@ app = FastAPI(title="Lazy Weather API")
 @app.get("/")
 def root():
     return {"message": "Lazy Weather API is running"}
+
+@app.get("/weather-advice/{city}")
+def get_weather_advice(city: str):
+    return {
+        "city": "Berkeley",
+        "today_temp_c": 18,
+        "yesterday_temp_c": 15,
+        "difference_c": 3,
+        "comparison": "warmer",
+        "advice": "Today is a little warmer than yesterday. A light layer should be enough."
+        }
